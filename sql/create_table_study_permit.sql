c0679a1a4b140bffaedfc3b6eb81558a445a7b7a
Savan Patel <51812887+Savan2110@users.noreply.github.com>
2025-10-24 13:34:53 -0400

    Add files via upload

--- sql/create_table_study_permit.sql
+++ sql/create_table_study_permit.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE dbo.StudyPermitFinal
+(
+    Year INT,
+    Quarter NVARCHAR(20),
+    Month NVARCHAR(20),
+    Gender NVARCHAR(50),
+    Total INT,
+    LoadTimestamp DATETIME2 DEFAULT SYSUTCDATETIME()
+);
